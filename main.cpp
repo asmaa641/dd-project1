def15aa571c4e339ef28c75528fc59ddce4541b1
Asmaa Elsayed Elshabshiri <asmaaelshabshiri@aucegypt.edu>
2025-11-30 08:40:34 +0200

    added more testing

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char* argv[]) {
     filesystem::path exePath = filesystem::absolute(argv[0]).parent_path();
     ifstream file;
     
-    file.open(exePath / "4_input_allPI_are_EPI.txt");
+    file.open(exePath / "4_input_somePI_are_EPI.txt");
     
     if (!file.is_open()) { //to ensure the file opens
             cout << "Error: Unable to open file!" << endl;
@@ -53,7 +53,7 @@ int main(int argc, char* argv[]) {
         return 1;
     }
     string line12;
-    if(!getline(file2, line12) && (line2.substr(0,1)!="m" || line2.substr(0,1)!="M")){
+    if(!getline(file2, line12) && (line12.substr(0,1)!="m" || line12.substr(0,1)!="M")){
         cout<<"Missing second line"<<endl;
         return 1;
     }
@@ -62,7 +62,33 @@ int main(int argc, char* argv[]) {
         cout<<"Missing third line"<<endl;
         return 1;
     }
+
+    ifstream file3;
+    file3.open(exePath / "20_input_somePI_are_EPI");
     
+    if (!file3.is_open()) { //to ensure the file opens
+            cout << "Error: Unable to open file!" << endl;
+            return 1;
+        }
+    
+    string line21; //to get the size from the file and initialize it to the PI
+    
+    if(!getline(file3, line21)){
+        cout<<"Missing first line"<<endl;
+        return 1;
+    }
+    string line22;
+    if(!getline(file3, line22) && (line22.substr(0,1)!="m" || line22.substr(0,1)!="M")){
+        cout<<"Missing second line"<<endl;
+        return 1;
+    }
+    string line23;
+    if(!getline(file3, line23)){
+        cout<<"Missing third line"<<endl;
+        return 1;
+    }
+    
+    cout << "This is the test case of 4 input some PI are EPI:" << endl;
     QM trial(stoi(line1),line2,line3);
     trial.generatePI();
     trial.displayPI();
@@ -72,8 +98,8 @@ int main(int argc, char* argv[]) {
 
     file.close();
 
-    
-    QM trial2(stoi(line12),line12,line13);
+    cout << "This is the test case of 10 input some PI are EPI:" << endl;
+    QM trial2(stoi(line11),line12,line13);
     trial2.generatePI();
     trial2.displayPI();
     trial2.generateSolutions();
@@ -82,7 +108,14 @@ int main(int argc, char* argv[]) {
 
     file2.close();
 
-    
+    cout << "This is the test case of 20 input some PI are EPI:" << endl;
+    QM trial3(stoi(line21),line22,line23);
+    trial3.generatePI();
+    trial3.displayPI();
+    trial3.generateSolutions();
+    trial3.displayEPI();
+    trial3.displaySolutions();
 
+    file3.close();
     return 0;
 }
